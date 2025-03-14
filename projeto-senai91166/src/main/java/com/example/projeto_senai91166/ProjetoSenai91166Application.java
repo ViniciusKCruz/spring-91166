ba61f0584bb9777d731d1ed3073bd636fed370f4
Aluno Dendezeiros <aluno.den@senaiba.com.br>
2025-03-14 19:54:29 -0300

    first commit

---
+++ projeto-senai91166/src/main/java/com/example/projeto_senai91166/ProjetoSenai91166Application.java
@@ -0,0 +1,13 @@
+package com.example.projeto_senai91166;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSenai91166Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSenai91166Application.class, args);
+	}
+		
+}
---
+++ projeto-senai91166/src/test/java/com/example/projeto_senai91166/ProjetoSenai91166ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_senai91166;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSenai91166ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
